187af99f7a503f551b1e536ca8bfdb6b5505471e
Pluto <qwtrplx@gmail.com>
2020-12-14 20:39:23 -0600

    Nav Boxes 0

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import '../index.css';
 
 const Nav = () => {
     return (
         <nav>
-            <Link className="nav-link" to="/playground">
-                <h3>Logo</h3>
-            </Link>
+            <NavLink className="nav-link" to="/playground">
+                <li>Logo</li>
+            </NavLink>
             <ul className="nav-links">
-                <Link className="nav-link" to="/playground/about">
+                <NavLink className="nav-link" to="/playground/about">
                     <li>About</li>
-                </Link>
-                <Link className="nav-link" to="/playground/monsters">
+                </NavLink>
+                <NavLink className="nav-link" to="/playground/monsters">
                     <li>Monsters</li>
-                </Link>
+                </NavLink>
             </ul>
         </nav>
     );
